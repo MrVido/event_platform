0c72fae187feac80fc7e802c0e092653f10435ff
MrVido <zvonividovic@gmail.com>
2023-12-19 19:16:11 -0500

    modified:   lib/database/index.ts

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,7 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'event_planner2.0',
+    dbName: 'evently',
     bufferCommands: false,
   })
 
